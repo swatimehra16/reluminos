4b637bd879e8e7e3da17387a2aa6861240076720
swatimehra16 <swatimehra91@gmail.com>
2023-03-27 00:40:55 +0530

    initial commit for users table

---
+++ components/userData.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-data-table :headers="headers" :items="users">
+    <template #[`item.actions`]="{ item }">
+      <v-btn @click="changeRole(item)">Set as</v-btn>
+    </template>
+  </v-data-table>
+</template>
+<script>
+export default {
+  name: 'UserData',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Name',
+          value: 'name',
+        },
+        {
+          text: 'Email',
+          value: 'email',
+        },
+        {
+          text: 'Role',
+          value: 'role',
+        },
+        {
+          text: 'Actions',
+          value: 'action',
+        },
+      ],
+    }
+  },
+  computed: {
+    users() {
+      return this.$store.getters.users
+    },
+  },
+}
+</script>
--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <h1>Welcome</h1>
+    <!-- <div v-if="usersRole === 'admin'"><UserData /></div> -->
   </div>
 </template>
 <script>
+// import UserData from '@/components/userData.vue'
+
 export default {
+  // components: { UserData },
   middleware: ['auth'],
+  computed: {
+    usersRole() {
+      return this.$store.getters.role
+    },
+  },
 }
 </script>
